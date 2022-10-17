9998c20ca1e555ae374e1c3a6260584ac1655adc
August McAllister <august.chm@gmail.com>
2022-10-17 15:57:32 -0500

    Set up project, added first function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,16 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addtoCollection(title, artist, yearPublished) {
+  
+  let obj = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+
+  collection.push(obj);
+
+  return obj;
+}
